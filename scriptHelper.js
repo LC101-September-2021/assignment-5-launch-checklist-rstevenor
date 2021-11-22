4719e29d175b59105a63e04ab6f0803626f830be
Rosalyn <rosalyn.stevenor@gmail.com>
2021-11-22 13:01:40 -0600

    trying a continue condition on the loop to see if that might prevent from being needed for valid inputs

--- scriptHelper.js
+++ scriptHelper.js
@@ -29,16 +29,12 @@ function validateInput(testInput) {
     }
  }
 
+ let fieldValidation = (fieldlabels) => {
+     
+ }
+
  // Validates and utilizes user input data to determine shuttle readiness.
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    let launchStatus = document.getElementById("launchStatus");
-    let pilotName = document.getElementById("pilotStatus"); 
-    let copilotName = document.getElementById("copilotStatus");
-    let fuelStatus = document.getElementById("fuelStatus");
-    let cargoStatus = document.getElementById( "cargoStatus");
-    let fuel = Number(fuelLevel.value);
-    let cargo = Number(cargoLevel.value);
-    
     let fieldLabels = [pilot, copilot, fuelLevel, cargoLevel]
     let validInputs = {
         pilot: "Not a Number",
@@ -49,46 +45,52 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     let validInputsKey = ['pilot', 'copilot', 'fuelLevel', 'cargoLevel']
 
     // input validation
-
     for (i=0; i< fieldLabels.length; i++){
         let key = validInputsKey[i]
         let validated = validateInput(fieldLabels[i].value)
-        if(validated === validInputs[key]){
+        if (validated === validInputs[key]){
             continue;
-        } else if(validated === "Empty"){
-            proceed = false;
-            return "empty"
+        }else if(validated === "Empty"){
+            alert("All fields are required!");
+            return;
         }else if (validated !== validInputs[key]){
-
-            return "invalid"
+            alert("Make sure to enter valid information for each field!");
+            return;
         }
     }
-
-        list.style.visibility = "visible";
-        pilotName.innerHTML = `Pilot ${pilot.value} is ready for launch`
-        copilotName.innerHTML = `Co-pilot ${copilot.value} is ready for launch`
     
-        if (fuel < 10000 || cargo > 10000){
-            launchStatus.style.color = "rgb(199, 37, 78)"
-            launchStatus.innerHTML = "Shuttle Not Ready for Launch"
-        } else {
-            launchStatus.style.color = "rgb(65, 159, 106)"
-            launchStatus.innerHTML = "Shuttle Ready for Launch"
-        }
-    
-        if (fuel < 10000){
-            fuelStatus.innerHTML = "Fuel level too low for launch."
-        } else{
-                fuelStatus.innerHTML = "Fuel level high enough for launch"
-        }
-        if (cargo > 10000){
-            cargoStatus.innerHTML = "Cargo mass too heavy for launch"    
-        } else {
-                cargoStatus.innerHTML = "Cargo mass low enough for launch"
-        }    
+    let launchStatus = document.getElementById("launchStatus");
+    let pilotName = document.getElementById("pilotStatus"); 
+    let copilotName = document.getElementById("copilotStatus");
+    let fuelStatus = document.getElementById("fuelStatus");
+    let cargoStatus = document.getElementById( "cargoStatus");
 
-    
+    list.style.visibility = "visible";
 
+    let fuel = Number(fuelLevel.value);
+    let cargo = Number(cargoLevel.value);
+
+    pilotName.innerHTML = `Pilot ${pilot.value} is ready for launch`
+    copilotName.innerHTML = `Co-pilot ${copilot.value} is ready for launch`
+
+    if (fuel < 10000 || cargo > 10000){
+        launchStatus.style.color = "rgb(199, 37, 78)"
+        launchStatus.innerHTML = "Shuttle Not Ready for Launch"
+    } else {
+        launchStatus.style.color = "rgb(65, 159, 106)"
+        launchStatus.innerHTML = "Shuttle Ready for Launch"
+    }
+
+    if (fuel < 10000){
+        fuelStatus.innerHTML = "Fuel level too low for launch."
+    } else{
+            fuelStatus.innerHTML = "Fuel level high enough for launch"
+    }
+    if (cargo > 10000){
+        cargoStatus.innerHTML = "Cargo mass too heavy for launch"    
+    } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch"
+    }
 
 }
 
